db3aa6edc5a85864bc06a5a0696271d0e9946fa7
Dolfas <rodolfo.amorim@tecnico.ulisboa.pt>
2024-03-04 21:52:13 +0000

    Additional Changes 4/03/2023

--- Project.m
+++ Project.m
@@ -18,16 +18,24 @@ x_fft = fft(AudioX(:,1),length(AudioX));
 %plot(fftshift(abs(x_fft)))
 
 %9 notes at 325095 th sample 
-[c,lags] = xcorr(AudioX(176880:188574,1),AudioX(1:325095,1));
-stem(lags,c); 
+[auto_corr_y,lags] = xcorr(AudioX(30000:325100,1));%,AudioX(63740:325100,1));
+%auto_corr_y = auto_corr_y(length(AudioX(1:325100,1)):length(AudioX(1:325100,1))+3000);
+subplot(2,1,1),plot(AudioX(1:325100,1)) 
+subplot(2,1,2),plot(auto_corr_y(325100:length(auto_corr_y)))
+%subplot(3,1,3),stem(lags,auto_corr_y) set the subplot(3,...,...) in order
+%to show these 3 plots 
+%stem(lags,c); 
 
 %5
 N=144;
 %spectrogram(AudioX(1:325095,1), hann(N), 3*N/4, 4*N, fs, 'yaxis');
 %title('Sepctogram of the first 9 tones')
 
+
 %% Part 2
 
+%1
+%Tone 1: 38000 to 64000 (in relation to sample number)
 
 %% Part 3
 
